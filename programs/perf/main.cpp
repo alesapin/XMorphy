84e74fd3b44a97f5a15404a99f45a434905e95e1
alesapin <alesapin@gmail.com>
2021-07-24 15:18:34 +0300

    Speedup more

--- programs/perf/main.cpp
+++ programs/perf/main.cpp
@@ -31,14 +31,15 @@ int main(int argc, char *argv[]) {
     MorphemicSplitter morphemic_splitter;
     size_t tokens_total = 0;
     auto global_start = std::chrono::system_clock::now();
-    auto start = std::chrono::system_clock::now();
     JoinedModel joiner;
     //size_t average_ms = 0;
     std::vector<size_t> forms_size;
     size_t prev_tokens = 0;
     std::vector<size_t> forms_lengths;
+    size_t sum_ms = 0;
     do
     {
+        auto start = std::chrono::system_clock::now();
         std::string sentence;
         ssplitter.readSentence(sentence);
         if (sentence.empty())
@@ -52,6 +53,8 @@ int main(int argc, char *argv[]) {
         context_disamb.disambiguate(forms);
         for (const auto & form : forms)
             morphemic_splitter.split(form);
+
+        sum_ms += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - start).count();
 
         size_t sentence_length = 0;
         for (auto & form : forms)
@@ -67,12 +70,11 @@ int main(int argc, char *argv[]) {
 
         if (tokens_total - prev_tokens > 10000)
         {
-            size_t current_ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count();
-            std::cerr << tokens_total - prev_tokens << " tokens processed in " << current_ms << "ms" << " (avg sentence len: " << std::accumulate(forms_size.begin(), forms_size.end(), 0) / static_cast<double>(forms_size.size()) << ", avg word length: " << std::accumulate(forms_lengths.begin(), forms_lengths.end(), 0) / static_cast<double>(forms_lengths.size()) << ")" << std::endl;
-            start = std::chrono::system_clock::now();
+            std::cerr << tokens_total - prev_tokens << " tokens processed in " << sum_ms / static_cast<double>(1000) << "ms" << " (avg sentence len: " << std::accumulate(forms_size.begin(), forms_size.end(), 0) / static_cast<double>(forms_size.size()) << ", avg word length: " << std::accumulate(forms_lengths.begin(), forms_lengths.end(), 0) / static_cast<double>(forms_lengths.size()) << ")" << std::endl;
             forms_size.clear();
             forms_lengths.clear();
             prev_tokens = tokens_total;
+            sum_ms = 0;
         }
     } while(!ssplitter.eof());
 
