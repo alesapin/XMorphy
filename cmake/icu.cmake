599e7b9e65ef7f57dd40cf6a761d1e79bc89e8cd
alesapin <alesapin@gmail.com>
2022-02-05 17:18:26 +0300

    Fix build for fresh clang

--- cmake/icu.cmake
+++ cmake/icu.cmake
@@ -1,2 +1,5 @@
 set (ICU_LIBRARIES icui18n icuuc icudata)
+target_compile_options(icui18n PRIVATE -Wno-unused -Werror)
+target_compile_options(icuuc PRIVATE -Wno-unused -Werror)
+target_compile_options(icudata PRIVATE -Wno-unused -Werror)
 set (ICU_INCLUDE_DIR "${XMorphy_SOURCE_DIR}/contrib/icu/icu4c/source/common")
